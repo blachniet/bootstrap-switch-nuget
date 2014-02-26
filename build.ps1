1dea9ff7b11a928f41d21ab355582e73044785b3
Brian Lachniet <blachniet@gmail.com>
2014-02-26 09:38:04 -0500

    Added the Bootstrap Switch Less Source package

--- build.ps1
+++ build.ps1
@@ -1,19 +1,43 @@
-nuget update -self
-npm update npm -g
-npm install
+function Package-BootstrapSwitch {
+    $pkgDir = "pkg\Bootstrap.Switch"
+    $nuspec = "Bootstrap.Switch.nuspec"
 
-rm *.nupkg
+    mkdir "$pkgDir\Content\Scripts"
+    mkdir "$pkgDir\Content\Content\bootstrap-switch"
 
-$pkgDir = "pkg\Bootstrap.Switch"
-if (Test-Path "$pkgDir" -PathType Container) {
-	rm "$pkgDir" -Recurse -Force -ErrorAction SilentlyContinue
+    cp $nuspec "$pkgDir"
+    cp node_modules\bootstrap-switch\build\js\* "$pkgDir\Content\Scripts"
+    cp node_modules\bootstrap-switch\build\css\* "$pkgDir\Content\Content\bootstrap-switch" -Recurse
+
+    nuget pack "$pkgDir\$nuspec" -OutputDirectory pkg
 }
 
-mkdir "$pkgDir\Content\Scripts"
-mkdir "$pkgDir\Content\Content\bootstrap-switch"
+function Package-BootstrapSwitchLess {
+    $pkgDir = "pkg\Bootstrap.Switch.Less"
+    $nuspec = "Bootstrap.Switch.Less.nuspec"
 
-cp Bootstrap.Switch.nuspec "$pkgDir"
-cp node_modules\bootstrap-switch\build\js\* "$pkgDir\Content\Scripts"
-cp node_modules\bootstrap-switch\build\css\bootstrap* "$pkgDir\Content\Content\bootstrap-switch" -Recurse
+    mkdir "$pkgDir\Content\Scripts"
+    mkdir "$pkgDir\Content\Content\bootstrap-switch"
 
-nuget pack "$pkgDir\Bootstrap.Switch.nuspec"
+    cp $nuspec "$pkgDir"
+    cp node_modules\bootstrap-switch\build\js\* "$pkgDir\Content\Scripts"
+    cp node_modules\bootstrap-switch\src\less\* "$pkgDir\Content\Content\bootstrap-switch" -Recurse
+
+    nuget pack "$pkgDir\$nuspec" -OutputDirectory pkg
+}
+
+function Clean {
+    rm node_modules -Recurse -Force -ErrorAction SilentlyContinue
+    rm pkg -Recurse -Force -ErrorAction SilentlyContinue
+}
+
+function Update {
+    nuget update -self
+    npm update npm -g
+    npm install
+}
+
+Clean
+Update
+Package-BootstrapSwitch
+Package-BootstrapSwitchLess
